ceecd53601e156378388d2641e3bc9a03eb7b091
HoangNA1911 <anhhoangdl12@gmail.com>
2023-07-30 22:52:52 +0700

    response all data of tables Tour, Schedule, Vehicel, TourStartDate, Picture, Location,

--- project/tour/tour_serializer.py
+++ project/tour/tour_serializer.py
@@ -2,9 +2,35 @@ from rest_framework import serializers
 from app.models import *
 from rest_framework.views import APIView
 
+
+class TourStartSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=TourStartDate
+    fields= ['id','tour_id','start_date']
+    
+class VehicelSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=Vehicel
+    fields= ['id','vehicel_name']
+    
+class LocationSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=Location
+    fields= ['id','location_name']
+    
+
+
 class TourSerializer(serializers.ModelSerializer):
   class Meta:
-    model = Tour
-    fields = ['id', 'start_location_Id', 'end_location_Id', 'vehicel_Id', 'tour_name', 'price', 'time', 'detail', 'tag']
+      model = Tour
+      fields = ['id', 'start_location_Id', 'end_location_Id', 'vehicel_Id', 'tour_name', 'price', 'time', 'detail', 'tag']
   
-
+class ScheduleSerializer(serializers.ModelSerializer):
+  class Meta:
+      model = Schedule
+      fields = ['id','date','activity','location_id','tour_startdate_id']
+  
+class PictureSerializer(serializers.ModelSerializer):
+  class Meta:
+      model = Picture
+      fields = ['id','tour_id','picture']
