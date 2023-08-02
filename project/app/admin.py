03591ab5fb2de2b2c080410c3159fcfa8ff874e6
HoangNA1911 <anhhoangdl12@gmail.com>
2023-08-02 21:42:01 +0700

    Done feature recommend + search tour with end location and start date

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Tour,Vehicel,Location,TourStartDate,Picture,Schedule
+from .models import React,Tour,Vehicel,Location,TourStartDate,Picture,Account,Profile,Register,Schedule
 # Register your models here.
 # admin.site.register(React)
 admin.site.register(Tour)
