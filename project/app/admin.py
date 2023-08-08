75b3bc5a05fe84a6861f1a692f4f29fd40428701
HoangNA1911 <anhhoangdl12@gmail.com>
2023-08-08 23:25:18 +0700

    Revert "no message"
    This reverts commit 5a37f624e5f5f03501d5bc7a5692aad329ffc3d0.

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import React,Tour,Vehicle,Location,TourStartDate,Account,Profile,Register,Schedule
+from .models import React,Tour,Vehicel,Location,TourStartDate,Picture,Account,Profile,Register,Schedule
 # Register your models here.
 # admin.site.register(React)
 admin.site.register(Tour)
-admin.site.register(Vehicle)
+admin.site.register(Vehicel)
 admin.site.register(Location)
 admin.site.register(TourStartDate)
+admin.site.register(Picture)
 admin.site.register(Schedule)
 
 
